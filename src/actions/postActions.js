e63d40466c1cb2ff64a8370724f3728185e179cf
Alejandro <frozmiz@live.com>
2023-01-31 23:26:57 +0100

    feat: postActions created

---
+++ src/actions/postActions.js
@@ -0,0 +1,35 @@
+export const GET_POST = "GET_POST";
+export const GET_POST_OK = "GET_POST_OK";
+export const GET_POST_ERROR = "GET_POST_ERROR";
+
+export const actionGetPost = () => ({
+    type: GET_POST,
+});
+
+export const actionGetPostOk = (post) => ({
+    type: GET_POST_OK,
+    payload: post,
+}); 
+
+export const actionGetPostError = () => ({
+    type: GET_POST_ERROR,
+});
+
+function getPost(postId) {
+    return async (dispatch) => {
+
+        dispatch(actionGetPost());
+
+        try {
+            const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`);
+            const data = await res.json();
+
+            dispatch(actionGetPostOk(data));
+        } catch (error) {
+            console.log("El post no se ha cargado correctamente.")
+            dispatch(actionGetPostError());
+        }
+    }
+}
+
+export default getPost;
