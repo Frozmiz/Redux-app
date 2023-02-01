d9af60e4502a47f51c013a071f022128c7e197ca
Alejandro <frozmiz@live.com>
2023-02-01 16:40:57 +0100

    feat: route configuration (react-router-dom v6

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import Posts from './pages/Posts';
+import PostPage from './pages/PostPage';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     React + Redux = Sweet
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/posts' component={Posts}/>
+        <Route exact path='/posts/:postId' component={PostPage}/>
+        {/* react-router-dom v5: <Redirect to="/" /> */}
+        <Route  render={() => <redirect to= "/" />} /> 
+      </Routes>
+    </Router>
   );
 }
 
